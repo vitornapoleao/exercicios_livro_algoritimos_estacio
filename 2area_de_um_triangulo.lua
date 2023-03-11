088171b089b9ecd3d030388b1399e0688282080e
vitornapoleao <109608876+vitornapoleao@users.noreply.github.com>
2023-03-11 11:37:25 -0300

    Create 2area_de_um_triangulo.lua

---
+++ 2area_de_um_triangulo.lua
@@ -0,0 +1,9 @@
+--escreva um algoritimo computacional capaz de calcular 
+-- a area de um retangulo. para isso, utilize a seguinte formula:
+-- Área = (base * altura)
+io.write("insira a base do seu triangulo:")
+base = tonumber(io.read())
+io.write("insira altura do triangulo em questão:")
+altura = tonumber(io.read())
+area = base * altura
+print("a area do triangulo é:"..(area))
