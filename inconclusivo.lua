90bb26b0bfccf09b0afa04eb4b687b63a8546080
vitornapoleao <109608876+vitornapoleao@users.noreply.github.com>
2023-03-12 01:47:35 -0300

    Create inconclusivo.lua

---
+++ inconclusivo.lua
@@ -0,0 +1,15 @@
+--escreva um algoritimo computacional que receba quatro valores
+--do tipo inteiro e determine o menor elempento
+io.write("insira um numero inteiro:")
+n1 = tonumber(io.read())
+io.write("insira outro numero inteiro:")
+n2 = tonumber(io.read())
+io.write("insira outro numero inteir:")
+n3 = tonumber(io.read())
+io.write("insira so mais um inteiro:")
+n4 = tonumber(io.read())
+if n1 > n2 > n3 >4 then
+    print("O primeiro elemento é o menor elemento")
+    else 
+        print(" O primeiro numero não é o menor elemento")
+        end
