1883340a0d6360da2ff01d768b8b5c062b572cb4
vitornapoleao <109608876+vitornapoleao@users.noreply.github.com>
2023-03-12 15:45:16 -0300

    Update and rename inconclusivo.lua to menor_de_4.lua

--- menor_de_4.lua
+++ menor_de_4.lua
@@ -8,8 +8,13 @@ io.write("insira outro numero inteir:")
 n3 = tonumber(io.read())
 io.write("insira so mais um inteiro:")
 n4 = tonumber(io.read())
-if n1 > n2 > n3 >4 then
-    print("O primeiro elemento é o menor elemento")
-    else 
-        print(" O primeiro numero não é o menor elemento")
-        end
+
+if n1 < n2 and n1 < n3 and n1 < n4 then
+    print("O primeiro numero é o menor elemento")
+elseif n2 < n3 and n2 < n4 then
+        print("o segundo numero é o menor elemento")
+elseif n3 < n4 then
+        print("o terceiro numero é o menor elemento")
+    else
+        print("o quarto numero é o menor elemento")
+    end
